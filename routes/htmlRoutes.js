b7dbdc0f0111ec3dc5695c39eae25912239a86f9
Clay Jones <cgjones0711@gmail.com>
2021-05-06 18:55:54 -0500

    adjusted htmlworkout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,13 @@ const Workout = require("../models/workout.js");
 
 //excercise
 router.get("/excercise", (req, res) => {
+  Workout.find({})
   res.sendFile(path.join(__dirname,"../public/excercise.html"))
 });
 
 // stats
 
 router.get("/stats", (req, res) => {
+  Workout.find({})
     res.sendFile(path.join(__dirname,"../public/stats.html"))
   });
