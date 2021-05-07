d63b6d5c8ca47f40998af634800bd65c099b757e
Clay Jones <cgjones0711@gmail.com>
2021-05-06 19:22:00 -0500

    adjusted index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const htmlRoutes = require("./htmlRoutes");
+const statsRoutes = require("./stats");
+const exerciseRoutes = require("./exercise");
 
 router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
+router.use("/stats", statsRoutes);
+router.use("/exercise", exerciseRoutes);
 
 module.exports = router;
